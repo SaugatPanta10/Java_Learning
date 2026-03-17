4324c6762865564c35dbc575626ea38ff6a2a48e
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-17 17:45:41 +0545

    Learning about methods in java

--- README.md
+++
@@ -1,2 +0,0 @@
-# Java_Learning
-It consists of simplest of java programs just to learn 
--- testencapsulation.java
+++
@@ -1,7 +0,0 @@
-public class testencapsulation {
-    public static void main(String[] args) {
-        bank b = new bank();
-        b.setBalance(100);
-        System.out.println("My balance is" + b.getBalance());
-    }
-}
