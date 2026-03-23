45120527b345089c709cd73c0823337163bb4b98
SaugatPanta10 <pantasaugat102@gmail.com>
2026-03-23 21:57:02 +0545

    now, takes the user input and prints the multiplication table

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class MultiplicationTable {
     public static void main(String[] args) {
-        int num = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number that you the multiplication table of: ");
+        int num = sc.nextInt();
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + " x " + i + " = " + (num * i));
